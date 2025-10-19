4f469c8d27cf9b6d423681a4b7a6f2360103364a
Seredin Nikolay <kolyaseredin128@gmail.com>
2025-10-19 16:41:31 +0300

    fdffd

--- lab2_float/task3.cpp
+++ lab2_float/task3.cpp
@@ -6,21 +6,12 @@ int main() {
     cout.precision(1);
 
     float x = 1.0f;
-    unsigned long long i = 0;
-    while (true) {
+    while (x<16777300) {
         float next = x + 1.0f;
-        if (i>16777200) {
+        if (x>16777200) {
             cout << "x = " << x << ", x + 1 = " << next;
-            cout << ", разница = " << (next - x) << endl;
-        }
-        if (next == x) {
-            cout << "x = " << x << endl;
-            cout << "x + 1 = " << next << endl;
-            cout << "Прибавление 1 не изменяет значение!" << endl;
-            break;
-        }
+            cout << ", разница = " << (next - x) << endl;}
         x += 1.0f;
-        i+=1;
     }
     return 0;
 }
